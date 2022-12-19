8ef467d38e1bcb50afc0c01a70dd30b1055f1ad4
aksherap <111791288+aksherap@users.noreply.github.com>
2022-12-19 12:09:24 -0800

    Update index.php

--- index.php
+++ index.php
@@ -3,20 +3,24 @@
   <head>
 </head>
   <script src="style.js"></script>
+ //Title of php Page
 <body>
   <h1>USERS PAGE</h2>
 
 <?php
+ //connects to database
 include "connect.php";
+ //finds all users in database
 include "keyword.php";
 ?>
-
+//form is shown to user so they can fill it out
 <form action="specific.php">
   Please enter a letter/first name to search for:<br>
   <input type="text" name="keyword"><br>
   <input type="submit" value="Submit">
 </form>
 <hr>
+ //form is shown to user so they can fill it out
 <form action="add_keyword.php">
 Please enter a first name here:<br>
   <input type="text" name="fname"><br>
@@ -29,6 +33,7 @@ Please enter a last name here:<br>
 
 
 <?php
+ //finds specific user that the person typed in the search
 include "specific.php";
 
 
